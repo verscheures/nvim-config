852d076c7dfeeca8f816d0ebc83b29f40adb8909
Stefaan Verscheure <verscheure.stefaan@gmail.com>
2023-11-11 20:21:42 +0100

    add neorg settings

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,14 +23,20 @@ require("lazy").setup({
         require("neorg").setup({
           load = {
             ["core.defaults"] = {}, -- Loads default behaviour
-            ["core.concealer"] = {}, -- Adds pretty icons to your documents
+            ["core.concealer"] = {
+              config = {
+                icon_preset = "diamond",
+              },
+            }, -- Adds pretty icons to your documents
             ["core.dirman"] = { -- Manages Neorg workspaces
               config = {
                 workspaces = {
-                  notes = "~/notes",
+                  notes = "~/Documents/notes",
                 },
+                default_workspace = "notes",
               },
             },
+            ["core.summary"] = {},
           },
         })
       end,
